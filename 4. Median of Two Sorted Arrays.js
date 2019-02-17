ce6cd82676474b6deee798f6772ee25e8f7142d5
Luís Alexandre Rodrigues Chaves <TheNerdGuyLulu@hotmail.com>
2019-02-17 04:39:50 +0000

    Create 4. Median of Two Sorted Arrays.js

---
+++ 4. Median of Two Sorted Arrays.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+let findMedianSortedArrays = function(nums1, nums2) {
+    nums1 = nums1.concat(nums2).sort(function(a,b){return a - b});
+
+    if (nums1.length % 2 > 0)
+        return nums1[Math.floor(nums1.length/2)];
+    else
+        return (nums1[nums1.length/2] + nums1[nums1.length/2-1]) / 2;
+};
