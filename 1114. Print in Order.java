43bf98c9dca571a1ac65893a766ca1e037f94171
Luís Chaves <luis.chaves@netguru.com>
2023-06-24 00:21:01 +0200

    fix: fix problem 1114

--- 1114. Print in Order.java
+++ 1114. Print in Order.java
@@ -1,28 +1,34 @@
 class Foo {
-    int i = 0;
-    
-    public Foo() {
-        
-    }
+    volatile int i = 0;
 
-    public void first(Runnable printFirst) throws InterruptedException {
-        while (i != 0);
-        // printFirst.run() outputs "first". Do not change or remove this line.
+    public synchronized void first(Runnable printFirst) throws 
+InterruptedException {
+        ++i;
+        // printFirst.run() outputs "first". Do not change or remove this 
+line.
         printFirst.run();
-        i = 1;
+        notifyAll();
     }
 
-    public void second(Runnable printSecond) throws InterruptedException {
-        while (i != 1);
-        // printSecond.run() outputs "second". Do not change or remove this line.
+    public synchronized void second(Runnable printSecond) throws 
+InterruptedException {
+        while(i != 1) {
+            wait();
+        }
+        ++i;
+        // printSecond.run() outputs "second". Do not change or remove 
+this line.
         printSecond.run();
-        i = 2;
+        notifyAll();
     }
 
-    public void third(Runnable printThird) throws InterruptedException {
-        while (i != 2);
-        // printThird.run() outputs "third". Do not change or remove this line.
+    public synchronized void third(Runnable printThird) throws 
+InterruptedException {
+        while(i != 2) {
+            wait();
+        }
+        // printThird.run() outputs "third". Do not change or remove this 
+line.
         printThird.run();
-        i = 3;
     }
 }
